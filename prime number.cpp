640be8541bc4b98d8a4ba372ba27258d68bab66e
Abhishek Rajpurohit <53573825+Abhiraj9757@users.noreply.github.com>
2019-09-11 21:59:47 +0530

    with comments

--- prime number.cpp
+++ prime number.cpp
@@ -3,21 +3,23 @@
 using namespace std;
 
 int main(){
+    //declaring variables
     int i,j,n,p;
-    for(i=1;i<=100;i++)
-    {
+    //using for loop to set range from 1 to 100
+    for(i=1;i<=100;i++){
     n=i;
     j=2;
     p=1;
-    while(j<=n/2)
-    {
-    if(n%j==0)
-    {
+        //while loop will check if the remainder is 0 or 1 if its 0 it will skip it and if the reminnder will 1 its a prime number
+    while(j<=n/2){
+    if(n%j==0){
               p=0;
               break;
               }
+        //j will increase the number and keep the loop working untill it reaches to 100
               j++;
     }
+        //when reminder is 0 output will be printed as prime number
     if(p==1)
     cout<<n<<",";
     }
